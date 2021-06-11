77c50c8252924f08d787cfef52d1f19cebf4c2dc
Yaya Laressa <yayalaressa2015@gmail.com>
2021-06-11 13:46:16 +0630

    Create index.php

---
+++ index.php
@@ -0,0 +1,11 @@
+<?php
+
+function translate($match) {
+  $e = 'd';
+	return str_replace($match[0], $match[0] . $e . $match[0], $match[0]);
+}
+
+$kata = "aku suka makan nasi goreng"; // text yang ingin diubah
+$result = preg_replace_callback('/(a|i|u|e|o|A|I|U|E|O)/i', 'translate', $kata);
+
+print($result); // output: adakudu sudukada madakadan nadasidi godoredeng
